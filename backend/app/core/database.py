836b36586ef72fe588b4f34e5e234e35f0549880
hasanmehediii <mehedi200105075@gmail.com>
2025-10-08 11:12:30 +0600

    add

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -4,16 +4,13 @@ from .config import settings
 client: AsyncIOMotorClient | None = None
 
 async def init_db():
-    global client
-    client = AsyncIOMotorClient(settings.MONGO_URI)
+    pass
 
 def get_db():
     global client
     if client is None:
-        raise Exception("Database client not initialized. Call init_db() first.")
+        client = AsyncIOMotorClient(settings.MONGO_URI)
     return client[settings.DB_NAME]
 
 def close_db():
-    global client
-    if client:
-        client.close()
+    pass
